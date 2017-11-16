f96bda7365bc25732ae682317623586865767fcb
OrDuan <orduani@gmail.com>
2017-11-16 22:16:19 +0200

    first test

--- src/tests/test.js
+++ src/tests/test.js
@@ -1,28 +1,51 @@
 const puppeteer = require('puppeteer')
+const chai = require('chai')
+const expect = chai.expect
+
 const sleep = ms => {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
+// couldn't use the native searchElm.click() it's just didn't work for most of the links
+const click = async (page, selector) => {
+  await page.evaluate(selector => {
+    document.querySelector(selector).click()
+  }, selector)
+}
 
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: [
-      '--disable-extensions-except=../../build',
-    ],
+describe('Extension', function () {
+  describe('Search', function () {
+    it('Should scroll to the target', async function () {
+      this.timeout(10000)
+      const browser = await puppeteer.launch({
+        headless: false,
+        args: [
+          '--disable-extensions-except=build/',
+        ],
+      })
+      const page = await browser.newPage()
+      await page.goto('https://www.google.co.il/search?q=random+text&oq=random+text&aqs=chrome..69i57j69i60j0j69i59j0l2.1392j0j9&sourceid=chrome&ie=UTF-8')
+      let searchResults = await page.$$('.rc')
+      for (let searchElm of searchResults) {
+        let url = await page.evaluate(e => e.innerText, await searchElm.$('._Rm'))
+        let grayLine = await searchElm.$('a[data-target-search-url]')
+        if (url === 'https://www.lipsum.com/') {
+          console.log(await page.evaluate(e => e.getAttribute('data-target-search-url'), await grayLine))
+
+          await click(page, `a[data-target-search-url="${url}"]`)
+          break
+        }
+      }
+      await page.waitForSelector('.targetsearch-icon-sm')
+      await sleep(100) // Scrolling has some delay
+
+      let scrollTop = await page.evaluate(() => window.scrollY)
+      let targetElement = await page.evaluate(() => document.querySelectorAll('.targetsearch-icon-sm').length)
+
+      expect(scrollTop).to.be.above(0)
+      expect(targetElement).to.be.equal(1)
+      await browser.close()
+
+
+    })
   })
-  const page = await browser.newPage()
-  await page.goto('https://www.google.com/search?q=game+of+thrones&oq=game&aqs=chrome.1.69i57j0l5.1815j0j4&sourceid=chrome&ie=UTF-8')
-  let searchResults = await page.$$('.rc')
-  for (let searchElm of searchResults) {
-    let url = await page.evaluate(e => e.innerText, await searchElm.$('._Rm'))
-    let grayLine = await searchElm.$('a[data-target-search-url]')
-    if (url === 'https://en.wikipedia.org/wiki/Game_of_Thrones') {
-      console.log(await page.evaluate(e => e.getAttribute('data-target-search-url'), await grayLine))
-
-      // couldn't use the native searchElm.click() it's just didn't work for most of the links
-      await page.evaluate(url=>{document.querySelector(`a[data-target-search-url="${url}"]`).click()}, url)
-    }
-  }
-  await sleep(10000)
-  await browser.close()
-})()
+})
