94b90f356071cf963862331081582eee1271a8ff
OrDuan <orduani@gmail.com>
2017-11-27 09:39:29 +0200

    refactoring assets/pages, added welcome tutorial

--- src/assets/js/background.js
+++ src/assets/js/background.js
@@ -5,6 +5,7 @@ import * as Raven from 'raven-js'
 (async function() {
   if (process.env.NODE_ENV === 'production') {
     let userData = await StorageManager.getUserData()
+
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -27,10 +28,21 @@ function onReady() {
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     ga(...request.params)
   });
+
+  // Check whether new version is installed
+  chrome.runtime.onInstalled.addListener(function (details) {
+    console.log('onInstalled')
+    if (details.reason === 'install') {
+      chrome.tabs.create({
+        url: chrome.runtime.getURL('assets/pages/welcome.html'),
+      })
+    } else if (details.reason === 'update') {
+      let thisVersion = chrome.runtime.getManifest().version
+      console.log('Updated from ' + details.previousVersion + ' to ' + thisVersion + '!')
+    }
+  })
 }
 
 Raven.context(function () {
-  $(document).ready(() => {
-    onReady()
-  })
+  onReady()
 })
